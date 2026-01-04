5d540a58a3bb34f141987fac25e1def07ea56803
Noe0131 <KTC24A31G0017@edu.kyoto-tech.ac.jp>
2026-01-04 23:36:48 +0900

    スレッドの受信がまだ受信できていない

---
+++ src/Worker_thread/worker.rs
@@ -0,0 +1,5 @@
+use std::{sync::mpsc::{self, Receiver, Sender}, thread};
+
+pub fn try_revc() {
+    //  revcで待ってmsgを受け取る
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 use std::io;
 use std::sync::mpsc::{self, Receiver, Sender};
 
+mod Worker_thread;
 
-fn main()  {
-    let stdin = read_buffer();
+fn main() {
+    let (tx,rx) = mpsc::channel();
+
+   
     loop {
-        println!("> {}", stdin);
+        let thread_tx = tx.clone();
+        let command = read_buffer();
+        std::thread::spawn(move || {
+            match thread_tx.send(command) {
+                Ok(_) => println!(""),
+                Err(e) => println!("error: {:?}",e),
+            }
+        })
     }
+    
 }
 
 /// 標準入力
 fn read_buffer() -> String {
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).expect("Failed to read line.");
-    buffer.trim().to_string()
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).expect("Failed to read line.");
+    command.trim().to_string()
 }
